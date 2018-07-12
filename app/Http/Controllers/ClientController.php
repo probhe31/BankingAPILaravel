20909b93f47b8ed5b99084520cd91d9e04523336
bherring_paucar <bpaucar@inventarte.net>
2018-07-12 16:43:38 -0500

    add accounts

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -26,4 +26,34 @@ class ClientController extends Controller
     {
         return response()->json(['fist_name'=>'Bherring', 'last_name'=>'Paucar Bonifacio']);
     }
+
+    public function accounts(Request $request)
+    {
+        $account1 = new Account();
+        $account1->id = '1';
+        $account1->name = 'Cuenta sueldo';
+        $account1->number = '1452-1245-4544';
+        $account1->amount = '5000';
+
+        $account2 = new Account();
+        $account2->id = '1';
+        $account2->name = 'Cuenta sueldo';
+        $account2->number = '666-1245-4666';
+        $account2->amount = '1500';
+
+
+        $accounts = array($account1, $account2);
+
+        return response()->json($accounts);
+
+    }
 }
+
+class Account
+{
+    public $id;
+    public $name;
+    public $number;
+    public $amount;
+}
+
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post('/client', 'ClientController@login');
+Route::post('/accounts', 'ClientController@accounts');
 Route::post('/client/profile', 'ClientController@profile');
